d4d75fc2ef0fe7c3d57890903c761da8f8ffd607
Amber Volkmann <volkmannamber@gmail.com>
2020-05-05 11:26:23 -0500

    index.js async fuctions added

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    entry: './lib/components/index.js',
+    entry: ['babel-polyfill', './lib/components/index.js'],
     output: {
       path: path.resolve(__dirname, 'public'),
       filename: 'bundle.js'
     },
   module: {
     rules: [
-      { test: /\.js$/, use: 'babel-loader' }
+      { test: /\.js$/, exlude: /node_modules/, use: 'babel-loader' }
     ]
   }
 };
